62ec9f082ee310c25439ad46de74393d2e9e47ab
KostuX <goldiskiker@gmail.com>
2021-11-22 22:37:35 +0000

    WebGL
    just to try

--- Drone/Assets/Scripts/DetectCollision.cs
+++ Drone/Assets/Scripts/DetectCollision.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
- bool   isColliding;
-public GameMngr GameMngr_Script;
+    bool isColliding;
+    public GameMngr GameMngr_Script;
     float half_Of_playZone;
-       public GameObject playZone;
+    public GameObject playZone;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-          playZone = GameObject.Find("PlayZone");
-  GameMngr_Script = GameObject.Find("GameMngr").GetComponent<GameMngr>();
+
+        playZone = GameObject.Find("PlayZone");
+        GameMngr_Script = GameObject.Find("GameMngr").GetComponent<GameMngr>();
     }
 
     // Update is called once per frame
     void Update()
     {
-isColliding = false;
-    half_Of_playZone = playZone.transform.localScale.x / 2;
-destroy_FN();
+        isColliding = false;
+        half_Of_playZone = playZone.transform.localScale.x / 2;
+        destroy_FN();
 
     }
 
     private void OnTriggerExit(Collider other)
     {
         //-----------------------------------------------------------------------------------------------
-        if(isColliding) return;
+        if (isColliding) return;
         isColliding = true;
         //https://answers.unity.com/questions/738991/ontriggerenter-being-called-multiple-times-in-succ.html
-        
-        
+
+
         Destroy(gameObject);
 
         if (gameObject.CompareTag("Point_Item"))
-        {GameMngr_Script.score+=5; }
+        { GameMngr_Script.score += 5; }
 
         if (gameObject.CompareTag("Time_Item"))
-        {GameMngr_Script.score+=10; }
+        { GameMngr_Script.score += 10; }
 
         if (gameObject.CompareTag("Speed_Item"))
-        {GameMngr_Script.score+=15; }
+        { GameMngr_Script.score += 15; }
     }
     void destroy_FN()
     {
